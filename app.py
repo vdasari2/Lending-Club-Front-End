b16d1d7e2318f105758863042bb734cca19d692d
Vishnu Preetham Reddy Dasari <vishnupreethamreddy@gmail.com>
2024-08-10 23:09:43 -0400

    Added new input fields

--- app.py
+++ app.py
@@ -15,44 +15,64 @@ def index():
     if request.method == 'POST':
         firstname = request.form['firstname']
         lastname = request.form['lastname']
-        loanamount = request.form['loanamount']
+        loanamount = int(request.form['loanamount'])
         loanpurpose = request.form['loanpurpose']
-        ssn = request.form['ssn']
-        FICO = random.randint(600, 850)
-        inq_last_6mths = random.randint(0,6)
-        revol_bal = random.randint(0,100)
-        home_ownership = random.randint(0,1)
-        # Insert data into MongoDB
-    
-        collection.insert_one({'First Name': firstname, 'Last Name': lastname, 'SSN':ssn, 'Loan Amount': loanamount, 'Loan Purpose':loanpurpose, 'FICO':FICO,
-        'Inquiries in last 6 Months':inq_last_6mths,'Revolving Utilization':revol_bal,'Home Ownership':home_ownership})
+        ssn = int(request.form['ssn'])
+        dob = request.form['dob']
+        income = int(request.form['income'])
+        inq_last_6mths = int(request.form['inq'])
+        int_rate = float(request.form['intrate'])
+        if request.form['homeownership'] == 'Yes':
+            home_ownership = 0
+        elif request.form['homeownership'] == 'No':
+            home_ownership = 1
+
+        if request.form['debtsetlement'] == 'Yes':
+            debtsetlement = 1
+        elif request.form['debtsetlement'] == 'No':
+            debtsetlement = 1
+
+        if request.form['Loandisbursal'] == 'CASH':
+            Loandisbursal = 0
+        elif request.form['Loandisbursal'] == 'DIRECT PAY':
+            Loandisbursal = 1
+
+        if request.form['incomeverification'] == 'Not Verified':
+            incomeverification = 0
+        elif request.form['incomeverification'] == 'Source Verified':
+            incomeverification = 1
+        elif request.form['incomeverification'] == 'Source Verified':
+            incomeverification = 2
+        DTI = float(request.form['DTI'])
+        lfico = int(request.form['lfico'])
+        ofico = int(request.form['ofico'])
+        numtrades = int(request.form['numtrades'])
+        currbal = int(request.form['currbal'])
+
+        collection.insert_one({'First Name': firstname, 'Last Name': lastname, 'DOB':dob,'Income':income, 'SSN':ssn, 'Loan Amount': loanamount, 'Loan Purpose':loanpurpose,
+        'Inquiries in last 6 Months':inq_last_6mths,'Home Ownership':home_ownership,'Interest Rate':int_rate,'Income Verification':incomeverification,
+        'DTI':DTI,'fico_range_low':lfico,'Account Management FICO':ofico,'numtrades':numtrades,'currbal':currbal,'disbursement_method':Loandisbursal,'debt_settlement_flag':debtsetlement})
         
-        # return redirect(url_for('success'))
         return 'Success!'
     return render_template('index.html')
 
 @app.route('/delete', methods=['POST'])
 def delete_records():
     if request.method == 'POST':
-        # Delete all records from the database
         collection.delete_many({})
         return 'All records are deleted from the database!'
 
 @app.route('/search', methods=['POST'])
 def search_records():
     if request.method == 'POST':
-        ssn_v1 = request.form['ssn_v1'].strip()
-        print(type(ssn_v1))
+        ssn_v1 = int(request.form['ssn_v1'].strip())
+        print(ssn_v1)
         record = collection.find_one({'SSN': ssn_v1})
-        print(type(record))
         print(record)
         if record:
-            # Convert ObjectId to string
             record['_id'] = str(record['_id'])
-            # Return the record as JSON response
             return jsonify(record)
         else:
-            # If no matching record was found, return an appropriate response
             return jsonify({'message': 'No record found for the provided SSN.'}), 404
 
 @app.route('/success')
